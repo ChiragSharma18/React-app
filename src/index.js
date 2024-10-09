7c2a931bb396095cbf169319cc1534445fbc0038
Chirag Kumar Sharma <chirag.sharma5233@gmail.com>
2024-10-09 17:32:03 +0530

    Revert "JS setup testing"

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//document.addEventListener
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
