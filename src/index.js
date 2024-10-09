39b23a8d0737ae22d60036ab65579baf0f6cd95f
Chirag <chirag.sharma5233@gmail.com>
2024-10-09 17:09:06 +0530

    JS setup testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//document.addEventListener
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
